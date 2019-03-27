1f6db10577bfbf0e4b4ca48911f01b71a9235fdb
Simon Haga <1498414+sewil@users.noreply.github.com>
2019-03-27 16:10:24 +0100

    Calculate once per workday, not per work item. Only calculate workdays as expected time

--- missinghours.py
+++ missinghours.py
@@ -21,7 +21,6 @@ def debugprint(message):
 	if arguments['--debug']:
 		print(message)
 
-
 harvest_api_token = arguments['<harvest-api-token>']
 harvest_account_id = arguments['<harvest-account-id>']
 workday_token = '6beda5db0166cdf21a74ad4a85b6772c241e1e26'
@@ -29,6 +28,12 @@ debugprint(arguments)
 debugprint(harvest_api_token)
 debugprint(harvest_account_id)
 debugprint(workday_token)
+
+def isWorkday(date):
+	j = requests.get(f'http://api.arbetsdag.se/v1/dagar.json?fran={date}&till={date}&key={workday_token}&id=1234').json()
+	isWorkday = j['antal_helgdagar'] == 0
+	debugprint('{} {}'.format(date, 'is a workday' if isWorkday else 'is not a workday'))
+	return isWorkday
 
 now = datetime.datetime.now()
 daysInMonth = calendar.monthrange(now.year, now.month)[1]
@@ -47,19 +52,24 @@ month_hours = workdays * 8
 debugprint(month_hours)
 
 j = requests.get(
-    f'https://api.harvestapp.com/v2/time_entries?from={dateFrom}&to={dateTo}',
-    headers={
-        'Authorization': f'Bearer {harvest_api_token}',
-        'Harvest-Account-Id': harvest_account_id
-    }
+	f'https://api.harvestapp.com/v2/time_entries?from={dateFrom}&to={dateTo}',
+	headers={
+		'Authorization': f'Bearer {harvest_api_token}',
+		'Harvest-Account-Id': harvest_account_id
+	}
 ).json()
 
 debugprint(j['time_entries'])
 total_hours = 0
+expected_entries = {}
 for time_entry in j['time_entries']:
 	total_hours += time_entry['hours']
+	spent_date = time_entry['spent_date']
+	if spent_date not in expected_entries and isWorkday(spent_date):
+		expected_entries[spent_date] = time_entry
+
 debugprint(total_hours)
-expected_hours = min(month_hours, len(j['time_entries']) * 8)
+expected_hours = min(month_hours, len(expected_entries) * 8)
 debugprint(expected_hours)
 hours_diff = total_hours-expected_hours
 debugprint(hours_diff)
@@ -68,7 +78,7 @@ debugprint(hours_diff_total)
 debugprint('')
 
 if hours_diff != 0 or hours_diff_total != 0:
-    print('You are {:0.2f} hours {} schedule. {:0.2f} hours {} for the entire month.'.format(
+	print('You are {:0.2f} hours {} schedule. {:0.2f} hours {} for the entire month.'.format(
 		hours_diff,
 		'ahead of' if hours_diff > 0 else 'behind',
 		hours_diff_total,
